02c7ffc866faa2d58235f5b6e199561abb475e07
Max Silva <maxsilvauk@gmail.com>
2018-10-13 05:32:19 +0100

    Hover effect on movie list.

--- src/components/Poster.jsx
+++ src/components/Poster.jsx
@@ -2,12 +2,11 @@ import React from 'react';
 import {URL_IMG, IMG_SIZE_LARGE} from '../const'
 import {Image} from 'react-bootstrap'
 import styled from 'styled-components'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSmile, faPercent } from '@fortawesome/free-solid-svg-icons'
+import {library} from '@fortawesome/fontawesome-svg-core'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faSmile, faPercent} from '@fortawesome/free-solid-svg-icons'
 
 library.add({faSmile, faPercent})
-
 
 export default function Poster(props) {
 
@@ -27,21 +26,24 @@ export default function Poster(props) {
     position: absolute;
     bottom: 4%;
     left: 5%;
-    color:white;
-    opacity:0;
+    color: white;
+    opacity: 0;
     z-index: 2;
   `;
-
-  // const listGenres = props.genres.map((genre) => <li>{genre}</li>);
-  // console.log('LIST', listGenres)
 
   const Overlay = styled.div `
     background: linear-gradient(to bottom, transparent 0%, black 99%);
     z-index: 1;
     position: absolute;
     height: 100%;
-    width: 101%;
+    width: 100%;
     display: block;
+
+    &:hover, &:focus {
+      opacity: 0;
+      transition: opacity 0.5s;
+      -webkit-transition: opacity 0.5s;
+    }
   `;
 
   const popularity = {
@@ -54,9 +56,10 @@ export default function Poster(props) {
 
   return (<StyledImg>
     <Overlay></Overlay>
-    <Image className="image" key={props.id} src={URL_IMG + IMG_SIZE_LARGE + props.path} responsive="true"/> {
+    <Image key={props.id} src={URL_IMG + IMG_SIZE_LARGE + props.path} responsive="true"/> {
       props.info && <Info className="title">
-          <span style={popularity}><FontAwesomeIcon icon="smile" /> {Math.round(props.popularity)} <FontAwesomeIcon icon="percent" style={percent} /></span>
+          <span style={popularity}><FontAwesomeIcon icon="smile"/> {Math.round(props.popularity)}
+            <FontAwesomeIcon icon="percent" style={percent}/></span>
           <h4>{props.title}</h4>
         </Info>
     }
