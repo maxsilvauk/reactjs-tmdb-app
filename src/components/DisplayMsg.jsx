4225f9fafa9f7f8ece4a30c0b10b6a8ea03fa556
Max Silva <maxsilvauk@gmail.com>
2018-10-16 01:17:21 +0100

    Updated fading timeout and display message

--- src/components/DisplayMsg.jsx
+++ src/components/DisplayMsg.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faDizzy } from '@fortawesome/free-solid-svg-icons'
+import styled from 'styled-components'
+import { Image } from 'react-bootstrap/lib'
+import logo from '../assets/images/themoviedb_green.svg'
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
-
-library.add({faDizzy})
 
 export default function DisplayMsg(props) {
 
@@ -18,7 +16,8 @@ export default function DisplayMsg(props) {
     position: 'fixed',
     top: '50%',
     left: '50%',
-    transform: 'translate(-50%, -50%)'
+    transform: 'translate(-50%, -50%)',
+    zIndex: 3
   }
 
   const displayMsgTitleStyle = {
@@ -27,7 +26,8 @@ export default function DisplayMsg(props) {
     color: '#30d673',
     marginTop: '10px',
     fontWeight: 'bold',
-    textAlign: 'center'
+    textAlign: 'center',
+    zIndex: 3
   }
 
   const displayMsgBodyStyle = {
@@ -36,23 +36,38 @@ export default function DisplayMsg(props) {
     color: 'white',
     marginTop: '10px',
     width: '230px',
-    textAlign: 'center'
+    textAlign: 'center',
+    zIndex: 3
   }
+
+  const Overlay = styled.div `
+    background:rgba(32,35,42,0.8);
+    z-index: 2;
+    position: fixed;
+    top:0;
+    left:0;
+    height: 100%;
+    width: 100%;
+    display: block;
+  `;
 
   if (!props.hasOwnProperty('message')) {
     return (
-      <ReactCSSTransitionGroup
-          transitionName="fade"
-          transitionAppearTimeout={800}
-          transitionEnterTimeout={800}
-          transitionLeaveTimeout={800}
-          transitionAppear={true}>
-          <div style={displayMsgIconStyle}>
-            <FontAwesomeIcon style={iconStyle} icon="dizzy" key="displayMsgIcon" />
-            <p style={displayMsgTitleStyle} key="displayMsgTitle">Whooops!</p>
-            <p style={displayMsgBodyStyle} key="displayMsgBody">TMDB API is being really slow or there have been too many requests. Click <a href="/">here</a> to retry.</p>
-          </div>
-      </ReactCSSTransitionGroup>
+      <div>
+        <ReactCSSTransitionGroup
+            transitionName="fade"
+            transitionAppearTimeout={400}
+            transitionEnterTimeout={400}
+            transitionLeaveTimeout={400}
+            transitionAppear={true}>
+            <div style={displayMsgIconStyle}>
+              <Image src={logo} id="logo" key="fade"/>
+              <p style={displayMsgTitleStyle} key="displayMsgTitle">Whooops!</p>
+              <p style={displayMsgBodyStyle} key="displayMsgBody">TMDB API is being really slow or there have been too many requests. Click <a href="/">here</a> to retry.</p>
+            </div>
+        </ReactCSSTransitionGroup>
+        <Overlay/>
+      </div>
     );
   } else {
     return (<div>{props.message}</div>);
